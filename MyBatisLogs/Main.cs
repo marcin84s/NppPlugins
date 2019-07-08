2fbebc1efb747d553230d1d27d5f071f23aabcf7
melvin <surma.marcin@gmail.com>
2019-07-08 21:25:37 +0200

    added replace parameters from selection

--- MyBatisLogs/Main.cs
+++ MyBatisLogs/Main.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Forms;
 using Kbg.NppPluginNET.PluginInfrastructure;
+using System.Text.RegularExpressions;
 
 namespace Kbg.NppPluginNET
 {
     class Main
     {
         internal const string PluginName = "MyBatisLogs";
-        static string iniFilePath = null;
-        static bool someSetting = false;
-        static frmMyDlg frmMyDlg = null;
-        static int idMyDlg = -1;
-        static Bitmap tbBmp = Properties.Resources.star;
-        static Bitmap tbBmp_tbTab = Properties.Resources.star_bmp;
-        static Icon tbIcon = null;
 
         public static void OnNotification(ScNotification notification)
         {  
@@ -34,73 +23,124 @@ namespace Kbg.NppPluginNET
 
         internal static void CommandMenuInit()
         {
-            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
-            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
-            iniFilePath = sbIniFilePath.ToString();
-            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
-            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
-            someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
-
-            PluginBase.SetCommand(0, "MyMenuCommand", myMenuFunction, new ShortcutKey(false, false, false, Keys.None));
-            PluginBase.SetCommand(1, "MyDockableDialog", myDockableDialog); idMyDlg = 1;
+            PluginBase.SetCommand(0, "Merge Parameters in New Tab", mergeParamsInNewTab, new ShortcutKey(true, true, false, Keys.P));
         }
 
-        internal static void SetToolBarIcon()
+        internal static void mergeParamsInNewTab()
         {
-            toolbarIcons tbIcons = new toolbarIcons();
-            tbIcons.hToolbarBmp = tbBmp.GetHbitmap();
-            IntPtr pTbIcons = Marshal.AllocHGlobal(Marshal.SizeOf(tbIcons));
-            Marshal.StructureToPtr(tbIcons, pTbIcons, false);
-            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_ADDTOOLBARICON, PluginBase._funcItems.Items[idMyDlg]._cmdID, pTbIcons);
-            Marshal.FreeHGlobal(pTbIcons);
-        }
+            IScintillaGateway scintillaGateway = PluginBase.GetGatewayFactory()();
 
-        internal static void PluginCleanUp()
-        {
-            Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
-        }
+            /*
+            Position posStart = scintillaGateway.GetSelectionStart();
+            Position posEnd = scintillaGateway.GetSelectionEnd();
 
+            int lineA = scintillaGateway.LineFromPosition(posStart);
+            int lineB = scintillaGateway.LineFromPosition(posEnd);
 
-        internal static void myMenuFunction()
-        {
-            MessageBox.Show("Hello N++!");
-        }
-
-        internal static void myDockableDialog()
-        {
-            if (frmMyDlg == null)
+            string line = scintillaGateway.GetLine(lineA);
+            */
+            string selection = scintillaGateway.GetSelText();
+            if (string.IsNullOrEmpty(selection))
             {
-                frmMyDlg = new frmMyDlg();
+                MessageBox.Show("no selection");
+            }
 
-                using (Bitmap newBmp = new Bitmap(16, 16))
+            try
+            {
+                string sql = FindInSelection(selection);
+                Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_MENUCOMMAND, 0, NppMenuCmd.IDM_FILE_NEW);
+
+                scintillaGateway.SetText(sql);
+            } 
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static string FindInSelection(string selection)
+        {
+            string preparingLabel = "==>  Preparing: ";
+            string parametersLabel = "==> Parameters: ";
+
+            int preparingIndex = selection.IndexOf(preparingLabel);
+            int parametersIndex = selection.IndexOf(parametersLabel);
+
+            if (preparingIndex == -1 || parametersIndex == -1 || preparingIndex > parametersIndex)
+            {
+                throw new Exception(String.Format("Cannot find either {0} or {1} in selection", preparingLabel, parametersIndex));
+            }
+
+            int endSql = selection.IndexOf("\r", preparingIndex);
+            if (endSql == -1 && endSql < parametersIndex)
+            {
+                throw new Exception(string.Format("Wrong position of end of line character after {}", preparingLabel));
+            }
+
+            int startSql = preparingIndex + preparingLabel.Length;
+            string sql = selection.Substring(startSql, endSql - startSql);
+
+            string parametersText = selection.Substring(parametersIndex + parametersLabel.Length);
+
+            Regex regex = new Regex(@"^(.*?\(\w+\))");
+
+            while (sql.Contains("?"))
+            {
+                if (parametersText.StartsWith("null"))
                 {
-                    Graphics g = Graphics.FromImage(newBmp);
-                    ColorMap[] colorMap = new ColorMap[1];
-                    colorMap[0] = new ColorMap();
-                    colorMap[0].OldColor = Color.Fuchsia;
-                    colorMap[0].NewColor = Color.FromKnownColor(KnownColor.ButtonFace);
-                    ImageAttributes attr = new ImageAttributes();
-                    attr.SetRemapTable(colorMap);
-                    g.DrawImage(tbBmp_tbTab, new Rectangle(0, 0, 16, 16), 0, 0, 16, 16, GraphicsUnit.Pixel, attr);
-                    tbIcon = Icon.FromHandle(newBmp.GetHicon());
+                    sql = ReplaceFirstOccurrence(sql, "?", "null");
+                    if (parametersText.Length > 6)
+                    {
+                        parametersText = parametersText.Substring(6);
+                    }
                 }
+                else
+                {
+                    Match match = regex.Match(parametersText);
+                    if (!match.Success)
+                    {
+                        throw new Exception(string.Format("regex match failed {0}", parametersText));
+                    }
 
-                NppTbData _nppTbData = new NppTbData();
-                _nppTbData.hClient = frmMyDlg.Handle;
-                _nppTbData.pszName = "My dockable dialog";
-                _nppTbData.dlgID = idMyDlg;
-                _nppTbData.uMask = NppTbMsg.DWS_DF_CONT_RIGHT | NppTbMsg.DWS_ICONTAB | NppTbMsg.DWS_ICONBAR;
-                _nppTbData.hIconTab = (uint)tbIcon.Handle;
-                _nppTbData.pszModuleName = PluginName;
-                IntPtr _ptrNppTbData = Marshal.AllocHGlobal(Marshal.SizeOf(_nppTbData));
-                Marshal.StructureToPtr(_nppTbData, _ptrNppTbData, false);
+                    Tuple<string, string> param = getParam(match);
+                    if (param.Item2 == "Integer" || param.Item2 == "Long")
+                    {
+                        sql = ReplaceFirstOccurrence(sql, "?", param.Item1);
+                    }
+                    else
+                    {
+                        sql = ReplaceFirstOccurrence(sql, "?", string.Format("'{0}'", param.Item1));
+                    }
 
-                Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_DMMREGASDCKDLG, 0, _ptrNppTbData);
+                    if (parametersText.Length > match.Length + 2)
+                    {
+                        parametersText = parametersText.Substring(match.Length + 2);
+                    }
+                }
             }
-            else
+
+            return sql;
+        }
+
+        private static string ReplaceFirstOccurrence(string Source, string Find, string Replace)
+        {
+            int Place = Source.IndexOf(Find);
+            string result = Source.Remove(Place, Find.Length).Insert(Place, Replace);
+            return result;
+        }
+
+        private static Tuple<string, string> getParam(Match match)
+        {
+            string[] param = match.Value.Split('(');
+            if (param.Length != 2)
             {
-                Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_DMMSHOW, 0, frmMyDlg.Handle);
+                throw new Exception(string.Format("wrong parameter {0}", match.Value));
             }
+            string value = param[0];
+            int endTypeIndex = param[1].IndexOf(')');
+            string type = param[1].Substring(0, endTypeIndex);
+
+            return new Tuple<string, string>(value, type);
         }
     }
 }
